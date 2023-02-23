7d568f0e7e9b675fa64859c1d3538b2f3a2ab8d3
Jon Eckerth <48563777+Jo-Eck@users.noreply.github.com>
2023-02-23 14:07:23 +0000

    adds Mynahs changes and the challenge description

--- challenge_02/challenge_02.c
+++ challenge_02/challenge_02.c
@@ -1,43 +1,19 @@
-
+#pragma config(Sensor, S2,     colorSensor,    sensorEV3_Color, modeEV3Color_Color)
+#pragma config(Sensor, S3,     sonarSensor,    sensorEV3_Touch)
+#pragma config(Sensor, S4,     gyroSensor,     sensorEV3_Gyro)
+#pragma config(Motor,  motorA,          armMotor,      tmotorEV3_Large, PIDControl, encoder)
+#pragma config(Motor,  motorB,          leftMotor,     tmotorEV3_Large, PIDControl, driveLeft, encoder)
+#pragma config(Motor,  motorC,          rightMotor,    tmotorEV3_Large, PIDControl, driveRight, encoder)
 
 void turn(int degrees);
 void move(int left,int right, int time);
-void hit_wall();
+void detect_wall(int min_distance);
+
+// Global variables
 int turn_speed = 50;
 int move_speed = 100;
 int reverse_speed = -30;
 TLegoColors target_colour = colorRed;
-
-
-task main()
-{
-	// Step 1
-   // Moving from zone1 to point 2 until we hit the Box and backing of a bit
-  hit_wall();
-    move(reverse_speed,reverse_speed, 500);
-
-    // Step 2
-    // turning 90 degrees to the right and moving towards the line
-    turn(80);
-
-
-}
-
-void move(int left,int right, int time){
-    // Moves the robot forward or backward
-    // int speed: The speed of the robot
-    // int time: The time the robot should move
-    // takes negative values for moving backward
-    // takes positive values for moving forward
-    // takes -1 for infinite time
-
-    setMotorSpeed(motorB, left);
-    setMotorSpeed(motorC, right);
-
-    if(time > -1){
-        delay(time);
-    }
-}
 
 void turn(int degrees){
     // Turns the robot by a certain amount of degrees
@@ -67,17 +43,37 @@ void turn(int degrees){
     move(0,0,0);
 }
 
+void move(int left,int right, int time){
+    // Moves the robot forward or backward
+    // int speed: The speed of the robot
+    // int time: The time the robot should move
+    // takes negative values for moving backward
+    // takes positive values for moving forward
+    // takes -1 for infinite time
 
-void hit_wall(){
-    // Moves the robot forward until it hits a wall
-    // If the robot hits a wall it will stop
+    setMotorSpeed(motorB, left);
+    setMotorSpeed(motorC, right);
 
-    bool button_pressed = false;
-
-    move(85,100, -1);
-    while (!button_pressed){
-        if (getTouchValue(touchSensor)==1){
-            button_pressed = true;
-        }
+    if(time > -1){
+        delay(time);
     }
 }
+
+void detect_wall(int min_distance){
+    //Stops robot a certain distance in front of a wall using sonar sensor
+    //Pass as parameter the distance in centimetres that you want the robot to stop at from the wall
+    while (SensorValue(sonarSensor) > min_distance){
+        move(move_speed,move_speed,-1);
+    }
+    move(0,0,500);
+}
+
+void detect_fire_uturn(TLegoColors target_colour){
+    
+}
+
+
+task main(){
+
+}
+
